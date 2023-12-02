14d6453c184259c4de16829d466186ee75016715
jhwmac <jkr2505@naver.com>
2023-12-02 17:14:25 +0900

    fix: defatulChecked 에러 대응

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 
+import Button from '@/components/inputs/button/Button';
+import CheckBox from '@/components/inputs/checkBox/CheckBox';
 import Radio from '@/components/inputs/radio/Radio';
 
 export default function Page() {
   return (
     <div>
-      <Radio name="a" disabled />
-      <Radio name="a" disabled />
-      <Radio name="a" disabled />
-      <Radio name="a" checked disabled />
+      <Radio id="1" name="a" disabled />
+      <Radio id="2" name="a" checked disabled />
     </div>
   );
 }
--- src/components/inputs/radio/Radio.tsx
+++ src/components/inputs/radio/Radio.tsx
@@ -1,32 +1,37 @@
+'use client';
 import styled from 'styled-components';
 
 interface RadioProps {
+  id: string;
   name: string;
   checked?: boolean;
   disabled?: boolean;
 }
 
-const Radio = ({ name, checked, disabled }: RadioProps) => {
-  return <Box type="radio" name={name} disabled={disabled} defaultChecked={checked} />;
+const Radio = ({ id, name, checked, disabled }: RadioProps) => {
+  return <Box type="radio" id={id} name={name} disabled={disabled} defaultChecked={checked} />;
 };
 
 const Box = styled.input`
-  width: 16px;
-  height: 16px;
-  appearance: none;
-  border-color: transparent;
-  background-image: url('/assets/svgs/RadioFalse.svg');
-  background-size: 100% 100%;
-  background-position: 50%;
-  background-repeat: no-repeat;
-  background-image: ${({ disabled, defaultChecked }) =>
-    defaultChecked && disabled && `url('/assets/svgs/RadioTrueDisabled.svg') !important`};
+  &[type='radio'] {
+    width: 16px;
+    height: 16px;
+    appearance: none;
+    border-color: transparent;
+    background-image: url('/assets/svgs/RadioFalse.svg');
+    background-size: 100% 100%;
+    background-position: 50%;
+    background-repeat: no-repeat;
 
-  &:disabled {
-    background-image: url('/assets/svgs/RadioFalseDisabled.svg');
-  }
-  &:checked {
-    background-image: url('/assets/svgs/RadioTrue.svg');
+    &:checked {
+      background-image: url('/assets/svgs/RadioTrue.svg');
+    }
+    &:disabled {
+      background-image: url('/assets/svgs/RadioFalseDisabled.svg');
+      &:checked {
+        background-image: url('/assets/svgs/RadioTrueDisabled.svg');
+      }
+    }
   }
 `;
 export default Radio;
