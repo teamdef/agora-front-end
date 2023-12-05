2544e322573c31dab4ec394fcafc739f5b65727c
jhwmac <jkr2505@naver.com>
2023-12-05 17:15:10 +0900

    fix: /provider.tsx ThemeProvider 삭제

--- src/components/common/Provider.tsx
+++ src/components/common/Provider.tsx
@@ -1,15 +1,11 @@
 'use client';
-import { theme } from '@/styles/theme';
 import StyledComponentsRegistry from '@/lib/registry';
 import ReactQueryProvider from '@/query/lib/ReactQueryProvider';
-import { ThemeProvider } from 'styled-components';
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
   return (
     <ReactQueryProvider>
-      <ThemeProvider theme={theme}>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
-      </ThemeProvider>
+      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
     </ReactQueryProvider>
   );
 };
