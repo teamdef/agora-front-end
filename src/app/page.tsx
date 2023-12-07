00868d5300c40b056e6f30dea017d435f4ec646f
jhwmac <jkr2505@naver.com>
2023-12-07 13:39:05 +0900

    feat: 버튼 스타일 로직 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 
 import ProfileImage from '@/components/common/profileImage';
 import Badge from '@/components/display/Badge';
+import Button from '@/components/inputs/button/Button';
 
 export default function Page() {
   return (
     <div>
       <ProfileImage />
+      <Button label="zz" outlined />
       <Badge label="시작 전" status="inProgress" />
     </div>
   );
--- src/components/inputs/button/Button.tsx
+++ src/components/inputs/button/Button.tsx
@@ -2,6 +2,7 @@
 
 import { styled } from 'styled-components';
 import { ReactNode } from 'react';
+import buttonPainter from '@/utils/buttonPainter';
 
 export interface ButtonProps {
   label: string;
@@ -38,80 +39,7 @@ const Box = styled.button<{ $style: ButtonStyleProps }>`
   }
   width: ${({ $style }) => ($style.small ? '120px' : '168px')};
   height: ${({ $style }) => ($style.small ? '36px' : '44px')};
-
-  ${({ $style, theme }) => {
-    if (!$style.disabled) {
-      if (!$style.outlined) {
-        return `
-          cursor: pointer;
-          background-color: ${theme.colors.agoraBlue[400]};
-          color: ${theme.colors.background};
-          & svg path {
-            fill: ${theme.colors.background};
-          }
-          &:hover {
-            background-color: ${theme.colors.agoraBlue[600]};
-            color: ${theme.colors.background};
-          }
-          &:active {
-            background-color: ${theme.colors.agoraBlue[700]};
-            color: ${theme.colors.background};
-          }
-        `;
-      } else {
-        return `
-          background-color: white;
-          cursor: pointer;
-          border: 1px solid ${theme.colors.agoraBlue[400]};
-          & span {
-            color: ${theme.colors.agoraBlue[400]};
-          }
-          & svg path {
-            fill: ${theme.colors.agoraBlue[400]};
-          }
-          &:hover {
-            border: 1px solid ${theme.colors.agoraBlue[600]};
-            & span {
-              color: ${theme.colors.agoraBlue[600]};
-            }
-            & svg path {
-              fill: ${theme.colors.agoraBlue[600]};
-            }
-          }
-          &:active {
-            background-color: ${theme.colors.agoraBlue[700]};
-            & span {
-              color: ${theme.colors.background};
-            }
-            & svg path {
-              fill: ${theme.colors.background};
-            }
-          }
-        `;
-      }
-    } else {
-      if (!$style.outlined) {
-        return `
-          cursor: default;
-          background-color: ${theme.colors.agoraBlue[100]};
-          color: ${theme.colors.background};
-          & svg path {
-            fill: ${theme.colors.background};
-          }
-        `;
-      } else {
-        return `
-          background-color: white;
-          cursor: default;
-          color: ${theme.colors.agoraBlack[600]};
-          border: 1px solid ${theme.colors.agoraBlack[600]};
-          & svg path {
-            fill: ${theme.colors.agoraBlack[600]};
-          }
-        `;
-      }
-    }
-  }}
+  ${({ $style }) => buttonPainter($style.outlined, $style.disabled)}
 `;
 
 export default Button;
