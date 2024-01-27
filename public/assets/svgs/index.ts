f810605f74a10f29899c5fb6a7de94eaee691f4c
jhwmac <jkr2505@naver.com>
2024-01-27 16:01:05 +0900

    chore: icon 네이밍 일괄 변경 (1)

---
+++ public/assets/svgs/index.ts
@@ -0,0 +1,95 @@
+import Alarm from './Alarm.svg';
+import ArrowDown from './ArrowDown.svg';
+import ArrowLeft from './ArrowLeft.svg';
+import ArrowRight from './ArrowRight.svg';
+import ArrowUp from './ArrowUp.svg';
+import ArrowFirst from './ArrowFirst.svg';
+import ArrowLast from './ArrowLast.svg';
+import Calendar from './Calendar.svg';
+import Cancel from './Cancel.svg';
+import Check from './Check.svg';
+import Delete from './Delete.svg';
+import Download from './Download.svg';
+import Enlarge from './Enlarge.svg';
+import Eye from './Eye.svg';
+import Favourites from './Favourites.svg';
+import File from './File.svg';
+import Folder from './Folder.svg';
+import Heart from './Heart.svg';
+import History from './History.svg';
+import History1 from './History1.svg';
+import Home from './Home.svg';
+import Icon from './Icon.svg';
+import Image from './Image.svg';
+import Info from './Info.svg';
+import Link from './Link.svg';
+import Mail from './Mail.svg';
+import More from './More.svg';
+import MoreVertical from './MoreVertical.svg';
+import NoEye from './NoEye.svg';
+import PaperClip from './PaperClip.svg';
+import PaperPlane from './PaperPlane.svg';
+import Pin from './Pin.svg';
+import Plus from './Plus.svg';
+import Portfeil from './Portfeil.svg';
+import Profile from './Profile.svg';
+import Search from './Search.svg';
+import Settings from './Settings.svg';
+import Share from './Share.svg';
+import TextAlignCenter from './TextAlignCenter.svg';
+import TextAlignJustified from './TextAlignJustified.svg';
+import TextAlignLeft from './TextAlignLeft.svg';
+import TextAlignRight from './TextAlignRight.svg';
+import CheckBoxFalse from './CheckBoxFalse.svg';
+import CheckBoxFalseDisabled from './CheckBoxFalseDisabled.svg';
+import CheckBoxTrue from './CheckBoxTrue.svg';
+import CheckBoxTrueDisable from './CheckBoxTrueDisabled.svg';
+
+export {
+  Alarm,
+  ArrowDown,
+  ArrowLeft,
+  ArrowRight,
+  ArrowUp,
+  ArrowFirst,
+  ArrowLast,
+  Calendar,
+  Cancel,
+  Check,
+  Delete,
+  Download,
+  Enlarge,
+  Eye,
+  Favourites,
+  File,
+  Folder,
+  Heart,
+  History,
+  History1,
+  Home,
+  Icon,
+  Image,
+  Info,
+  Link,
+  Mail,
+  More,
+  MoreVertical,
+  NoEye,
+  PaperClip,
+  PaperPlane,
+  Pin,
+  Plus,
+  Portfeil,
+  Profile,
+  Search,
+  Settings,
+  Share,
+  TextAlignCenter,
+  TextAlignJustified,
+  TextAlignLeft,
+  TextAlignRight,
+  CheckBoxFalse,
+  CheckBoxFalseDisabled,
+  CheckBoxTrue,
+  CheckBoxTrueDisable,
+};
