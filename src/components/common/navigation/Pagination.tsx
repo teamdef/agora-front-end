87a19e03d3116e83f61dc34b087d109724d5165f
jhwmac <jkr2505@naver.com>
2024-01-27 17:57:19 +0900

    feat: Pagination 컴포넌트 구성

--- src/components/common/navigation/Pagination.tsx
+++ src/components/common/navigation/Pagination.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { ArrowLeft } from 'public/assets/svgs';
-import { useEffect } from 'react';
+import { ArrowFirst, ArrowLast, ArrowLeft, ArrowRight } from 'public/assets/svgs';
 import styled from 'styled-components';
+import { theme } from '~/styles/theme';
 
 interface PaginationProps {
   totalPage: number;
@@ -15,37 +15,103 @@ const Pagination = ({ totalPage, currentPage, pagehandler }: PaginationProps) =>
   const arrLength = firstNum + 9 > totalPage ? totalPage - firstNum + 1 : 10;
   const pageArray = Array.from(new Array(arrLength), (x, i) => firstNum + i);
 
+  const firstPage = () => pagehandler(1);
+  const prevPage = () => pagehandler(currentPage - 10);
+  const nextPage = () => (currentPage + 10 > totalPage ? pagehandler(totalPage) : pagehandler(currentPage + 10));
+  const lastPage = () => pagehandler(totalPage);
+
   return (
     <Wrapper>
       <PrevButtonBox>
-        <ArrowLeft />
+        {firstNum !== 1 && (
+          <>
+            <ArrowFirst
+              width={16}
+              height={16}
+              viewBox="0 0 25 25"
+              color={theme.colors.agoraBlack[400]}
+              onClick={firstPage}
+            />
+            <ArrowLeft
+              width={16}
+              height={16}
+              viewBox="0 0 25 25"
+              color={theme.colors.agoraBlack[400]}
+              onClick={prevPage}
+            />
+          </>
+        )}
       </PrevButtonBox>
       <PaginationList>
-        {pageArray.map((item, idx) => {
+        {pageArray.map((num) => {
           return (
-            <PaginationItem $isActive={currentPage === item} onClick={() => pagehandler(item)}>
-              {item}
+            <PaginationItem $isActive={currentPage === num} onClick={() => pagehandler(num)}>
+              {num}
             </PaginationItem>
           );
         })}
       </PaginationList>
-      <NextButtonBox></NextButtonBox>
+      <NextButtonBox>
+        {pageArray[9] < totalPage && (
+          <>
+            <ArrowRight
+              width={16}
+              height={16}
+              viewBox="0 0 25 25"
+              color={theme.colors.agoraBlack[400]}
+              onClick={nextPage}
+            />
+            <ArrowLast
+              onClick={lastPage}
+              width={16}
+              height={16}
+              viewBox="0 0 25 25"
+              color={theme.colors.agoraBlack[400]}
+            />
+          </>
+        )}
+      </NextButtonBox>
     </Wrapper>
   );
 };
 const Wrapper = styled.div`
   position: relative;
   display: inline-flex;
+  justify-content: center;
+  align-items: center;
   gap: 10px;
+  svg {
+    cursor: pointer;
+  }
 `;
-const PrevButtonBox = styled.div``;
-const NextButtonBox = styled.div``;
+const PrevButtonBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 32px;
+  height: 24px;
+`;
+const NextButtonBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 32px;
+  height: 24px;
+`;
 const PaginationList = styled.ul`
   gap: 10px;
   display: flex;
 `;
 const PaginationItem = styled.li<{ $isActive: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 18px;
+  user-select: none; /* standard syntax */
+  -webkit-user-select: none; /* webkit (safari, chrome) browsers */
+  -moz-user-select: none; /* mozilla browsers */
+  -khtml-user-select: none; /* webkit (konqueror) browsers */
+  -ms-user-select: none; /* IE10+ */
   height: 24px;
   cursor: pointer;
   color: ${({ $isActive, theme }) => ($isActive ? theme.colors.agoraBlue[400] : theme.colors.agoraBlack[700])};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,15 @@
 import { useState } from 'react';
-import CheckBox from '~/components/common/inputs/checkBox/CheckBox';
 import Pagination from '~/components/common/navigation/Pagination';
 
 const Page = () => {
-  const [current, setCurrent] = useState(119);
-  const totalPage = 120;
+  const [current, setCurrent] = useState(110);
+  const totalPage = 115;
 
   const currentHandler = (idx: number) => {
     setCurrent(idx);
   };
   return (
-    <div>
-      <CheckBox id="a" checked />
+    <div style={{ padding: '100px' }}>
       <Pagination currentPage={current} pagehandler={currentHandler} totalPage={totalPage} />
     </div>
   );
