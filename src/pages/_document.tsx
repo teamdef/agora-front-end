88815f02f2ffda7733d30ab2ed1fcdacf4c9a555
hayoungzzzzz <jhy901@naver.com>
2024-01-27 12:53:03 +0900

    refact: app router 를 pages router로 migration

---
+++ src/pages/_document.tsx
@@ -0,0 +1,47 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2471989735930976"
+            crossOrigin="anonymous"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <div id="react-portal-modal-container" />
+          <div id="react-portal-loading-container" />
+        </body>
+      </Html>
+    );
+  }
+}
