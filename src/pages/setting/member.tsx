2da485170b67b819e0d586b9ca19fb7ffdbab316
hayoungzzzzz <jhy901@naver.com>
2024-01-28 21:27:48 +0900

    feat: 페이지 구성에 필요한 pages 컴포넌트 추가

---
+++ src/pages/setting/member.tsx
@@ -0,0 +1,17 @@
+import { ReactElement } from 'react';
+import GNBLayout from '~/components/layout/GNBLayout';
+import LNBLayout from '~/components/layout/LNBLayout';
+
+const Page = () => {
+  return <div>설정 - 멤버 준비중입니다.</div>;
+};
+
+Page.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <GNBLayout>
+      <LNBLayout>{page}</LNBLayout>
+    </GNBLayout>
+  );
+};
+
+export default Page;
