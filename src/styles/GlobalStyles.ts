cb9bb149f24956490f8398423a88ef5901f66953
hayoungzzzzz <jhy901@naver.com>
2024-01-28 21:28:10 +0900

    style: global style 에 user-select:none 옵션 추가

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -20,6 +20,11 @@ const styles = css`
       'Segoe UI Emoji',
       'Segoe UI Symbol',
       sans-serif;
+    user-select: none; /* standard syntax */
+    -webkit-user-select: none; /* webkit (safari, chrome) browsers */
+    -moz-user-select: none; /* mozilla browsers */
+    -khtml-user-select: none; /* webkit (konqueror) browsers */
+    -ms-user-select: none; /* IE10+ */
   }
   button {
     border: none;
